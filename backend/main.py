84bc88593bfce6f881f2dc18cb04d8fb7572237f
MatheusFariah <matheusfariaalmeida@hotmail.com>
2025-04-04 23:45:22 -0300

    Add 'backend' folder with all its content

---
+++ backend/main.py
@@ -0,0 +1,16 @@
+﻿# Este módulo define a aplicação Flask para o sistema FutStatics.
+# Ele inclui as rotas básicas para o backend, como a rota principal que
+# renderiza a página inicial (index.html).
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
